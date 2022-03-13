63b7b7ed4579ad6ddb9a72894db9048b1fa1f136
Thomas H <vhhorne2@gmail.com>
2022-03-13 12:27:34 -0400

    Fixed secret key leak (oops)

--- backend/instarDjango/instarDjango/settings.py
+++ backend/instarDjango/instarDjango/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import secretkey
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-39-7+5pbpnh^fhth3b5^47f5d=1z6&b7u_0t0#v6lz=9qc(zxt'
+SECRET_KEY = secretkey.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
