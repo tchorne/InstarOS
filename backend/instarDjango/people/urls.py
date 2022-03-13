f2756d2522b4d60d0ca0148a9bf1074ee7ed950a
Thomas H <vhhorne2@gmail.com>
2022-03-13 19:12:06 -0400

    Add notes functionality

--- backend/instarDjango/people/urls.py
+++ backend/instarDjango/people/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'people'
 urlpatterns = [
     # ex. /people/
     path('', views.index, name='index'),
     # ex. /people/5/
     path('<int:character_id>/', views.detail, name="detail"),
+
+    path('<int:character_id>/submitNote', views.submitNote, name="note"),
 ]
--- backend/instarDjango/people/views.py
+++ backend/instarDjango/people/views.py
@@ -1,13 +1,13 @@
-from tkinter.messagebox import QUESTION
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render
-from .models import Character
+from django.urls import reverse
+from .models import Character, Note
 
 
 def index(request):
-    character_list = Character.objects.order_by('-name')
+    character_list = Character.objects.order_by('-name')[::-1]
     context = {
         'character_list': character_list,
     }
@@ -15,6 +15,11 @@ def index(request):
 
 def detail(request, character_id):
     character = get_object_or_404(Character, pk=character_id)
+    notes_list = Note.objects.filter(character=character)
+    return render(request, 'people/detail.html', {'character':character, 'notes_list':notes_list})
 
-    return render(request, 'people/detail.html', {'character':character})
-
+def submitNote(request, character_id):
+    character = get_object_or_404(Character, pk=character_id)
+    new_note = Note(character = character, note_text = request.POST['note'])
+    new_note.save()
+    return HttpResponseRedirect(reverse('people:detail', args=(character_id,)))
