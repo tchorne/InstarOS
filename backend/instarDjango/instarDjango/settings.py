79008dbbdb3d989cfdca5f6a84bed3dbb940b2b5
Thomas H <vhhorne2@gmail.com>
2022-03-15 12:48:19 -0400

    debug off

--- backend/instarDjango/instarDjango/settings.py
+++ backend/instarDjango/instarDjango/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["instar-os.herokuapp.com", "https://instar-os.herokuapp.com"]
 CSRF_TRUSTED_ORIGINS = ["https://instar-os.herokuapp.com", "http://instar-os.herokuapp.com"]
