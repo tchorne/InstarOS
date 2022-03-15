5d5d71f3775afb30e04a7409cbd486f319c57ab2
Thomas H <vhhorne2@gmail.com>
2022-03-15 10:51:29 -0400

    port to int(port_

--- backend/instarDjango/run_production_server.py
+++ backend/instarDjango/run_production_server.py
@@ -10,6 +10,6 @@ PORT = os.environ['PORT']
 
 from django.core.handlers.wsgi import WSGIHandler as DjangoWSGIApp
 application = DjangoWSGIApp()
-server = WSGIServer(("127.0.0.1", PORT), application)
-print("Starting server on http://127.0.0.1:" + str(PORT))
+server = WSGIServer(("127.0.0.1", int(PORT)), application)
+print("Starting server on http://127.0.0.1:" + PORT)
 server.serve_forever()
