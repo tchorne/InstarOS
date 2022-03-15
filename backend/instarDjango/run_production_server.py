05d75b70c71380bb437c1ec05eb5bd40ee3e1993
Thomas H <vhhorne2@gmail.com>
2022-03-15 11:12:56 -0400

    back to 0.0.1

--- backend/instarDjango/run_production_server.py
+++ backend/instarDjango/run_production_server.py
@@ -7,11 +7,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instarDjango.settings")
 django.setup()
 
 PORT = os.environ['PORT']
-print(type(PORT))
+
 from django.core.handlers.wsgi import WSGIHandler as DjangoWSGIApp
 application = DjangoWSGIApp()
-server = WSGIServer(("127.0.0.0", int(PORT)), application)
+server = WSGIServer(("127.0.0.1", int(PORT)), application)
 
-print(os.environ)
-print("Starting server on http://127.0.0.0:" + PORT)
+print("Starting server on http://127.0.0.1:" + PORT)
 server.serve_forever()
