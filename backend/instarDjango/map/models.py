85e36c71bd26d1f01c25027d30f16cef3cc87ade
Thomas H <vhhorne2@gmail.com>
2022-03-17 10:03:13 -0400

    fix star_list still in html

--- backend/instarDjango/map/models.py
+++ backend/instarDjango/map/models.py
@@ -1,10 +1,11 @@
+from asyncio.windows_events import NULL
 from django.db import models
 
 # Create your models here.
 class Body(models.Model):
     name = models.CharField(max_length=200)
     imageUrl = models.URLField(max_length=200, default="https://cdn.discordapp.com/attachments/432564281113051136/952994827543404574/1355328252_2.gif")
-    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, required=False)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, default=NULL, blank=True)
     def __str__(self):
         return self.name
     def to_json(self):
