6963d24594f3ba8d38b5f63ab7e3e06277df4e8f
Thomas H <vhhorne2@gmail.com>
2022-03-17 14:25:52 -0400

    fix bad import in models.py

--- backend/instarDjango/map/models.py
+++ backend/instarDjango/map/models.py
@@ -1,11 +1,10 @@
-from asyncio.windows_events import NULL
 from django.db import models
 
 # Create your models here.
 class Body(models.Model):
     name = models.CharField(max_length=200)
     imageUrl = models.URLField(max_length=200, default="https://cdn.discordapp.com/attachments/432564281113051136/952994827543404574/1355328252_2.gif")
-    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, default=NULL, blank=True)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
     def __str__(self):
         return self.name
     def to_json(self):
