328f80a7bf28b983cd51f712dc2b4ec240c2c9f4
Thomas H <vhhorne2@gmail.com>
2022-03-21 11:06:46 -0400

    Add x and y positions to map

--- backend/instarDjango/map/models.py
+++ backend/instarDjango/map/models.py
@@ -5,6 +5,8 @@ class Body(models.Model):
     name = models.CharField(max_length=200)
     imageUrl = models.URLField(max_length=200, default="https://cdn.discordapp.com/attachments/432564281113051136/952994827543404574/1355328252_2.gif")
     parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+    x = models.FloatField(default=0)
+    y = models.FloatField(default=0)
     def __str__(self):
         return self.name
     def to_json(self):
